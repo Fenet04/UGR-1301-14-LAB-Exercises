528b4c4967d4b1e5c125666a9ecaaae9821941ab
Fenet04 <133098255+Fenet04@users.noreply.github.com>
2024-03-08 17:53:05 +0300

    Add files via upload

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String myName = "Fenet";
+  int myAge = 21; 
+  String myFavoriteColor = "mauve purple";
+  
+  print("My name is $myName.");
+  print("I am $myAge years old.");
+  print("My favorite color is $myFavoriteColor.");
+}
