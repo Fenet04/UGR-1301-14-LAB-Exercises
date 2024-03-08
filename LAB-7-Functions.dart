9210350934fcaff55829a5fd818e0154ad8a6906
Fenet04 <133098255+Fenet04@users.noreply.github.com>
2024-03-08 17:55:34 +0300

    Add files via upload

---
+++ LAB-7-Functions.dart
@@ -0,0 +1,10 @@
+void main() { 
+  double result = calculateSum(2, 5);
+  print("The sum of the numbers is: $result");
+}
+
+double calculateSum(double num1, double num2) {
+  return num1 + num2;
+}
+
+
